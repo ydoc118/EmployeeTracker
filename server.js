973c7fb6ccfca97ec612e7a72a402b1b1e75b7ca
Cody <ydoc118@gmail.com>
2020-08-12 18:52:20 -0400

    added prefilled employees to console.table

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
+//Installed packages
 const mysql = require("mysql");
+const cTable = require("console.table");
 const inquirer = require("inquirer");
 
-const connection = mysql.createConnection({
+
+//Setting up the connection
+var connection = mysql.createConnection({
     host: "localhost",
-    port: 3030,
+    port: 3306,
     user: "root",
     password: "yourRootPassword",
     database: "employee_trackerDB"
 });
 
-connection.connect(err => {
+//Connecting to the server
+connection.connect(function(err) {
     if(err) throw err;
     console.log("Connected as id " + connection.threadId);
-    
-})
+});
+
+//Function to prefill employee list
+function start() {
+    connection.query(`SELECT role.title, employee.first_name, employee.last_name, department.deptName, role.salary
+        FROM role
+        INNER JOIN employee
+        ON employee.role_id = role.id
+        INNER JOIN department
+        ON department.id = role.department_id AND role.id = employee.role_id
+        ORDER BY role.id;`, (err, res) => {
+    if(err) throw err;
+    console.table(res);
+});
+};
+
+start();
+
+
+
+
+
